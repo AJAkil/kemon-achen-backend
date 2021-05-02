3f65f9f8c0c95c6b4eed5a2283256c00c865b2f4
unknown <imshmen077@gmail.com>
2021-05-02 22:13:32 +0600

    test commit

--- controllers/post.js
+++ controllers/post.js
@@ -8,7 +8,7 @@ const Community = require("../models/Community");
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const mongoose = require("mongoose");
-const { getTimeDiff } = require("../utils/helperMethods");
+const {getTimeDiff} = require("../utils/helperMethods");
 
 /**
  * @desc     save a post by a logged in user
@@ -16,35 +16,33 @@ const { getTimeDiff } = require("../utils/helperMethods");
  * @access   Private
  */
 exports.savePost = asyncHandler(async (req, res, next) => {
-  let id = mongoose.Types.ObjectId(req.params.postId);
-  let message;
-  //console.log(typeof(id));
+	let id = mongoose.Types.ObjectId(req.params.postId);
+	let message;
+	//sconsole.log(typeof(id));
 
-  // check to see if the user belongs to the certain community
-  let post = await Post.find({
-    _id: id,
-  });
+	// check to see if the user belongs to the certain community
+	let post = await Post.find({
+		_id: id,
+	});
 
-  console.log(post);
+	console.log(post);
 
-  //console.log(req.user);
-  if (!post) {
-    return next(
-      new Error(`Post with id: ${req.user._id} is not on the database`, 400)
-    );
-  }
+	//console.log(req.user);
+	if (!post) {
+		return next(new Error(`Post with id: ${req.user._id} is not on the database`, 400));
+	}
 
-  if (req.query.saveOptions === "save") {
-    await User.findByIdAndUpdate(req.user.id, { $push: { savedPosts: id } });
+	if (req.query.saveOptions === "save") {
+		await User.findByIdAndUpdate(req.user.id, {$push: {savedPosts: id}});
 
-    message = "The Post has been saved!";
-  } else {
-    await User.findByIdAndUpdate(req.user.id, { $pull: { savedPosts: id } });
+		message = "The Post has been saved!";
+	} else {
+		await User.findByIdAndUpdate(req.user.id, {$pull: {savedPosts: id}});
 
-    message = "The Post has been unsaved!";
-  }
+		message = "The Post has been unsaved!";
+	}
 
-  res.status(200).json({ message: message });
+	res.status(200).json({message: message});
 });
 
 /**
@@ -53,37 +51,35 @@ exports.savePost = asyncHandler(async (req, res, next) => {
  * @access   Private
  */
 exports.likePost = asyncHandler(async (req, res, next) => {
-  //console.log(typeof(req.params.communityId));
+	//console.log(typeof(req.params.communityId));
 
-  let id = mongoose.Types.ObjectId(req.params.postId);
+	let id = mongoose.Types.ObjectId(req.params.postId);
 
-  let message = "";
-  let counter = 1;
+	let message = "";
+	let counter = 1;
 
-  // check to see if the user belongs to the certain community
-  if (req.query.likeOptions === "like") {
-    message = "The post has been liked!";
-  } else {
-    message = "The post has been unliked!";
-    counter = -1;
-  }
+	// check to see if the user belongs to the certain community
+	if (req.query.likeOptions === "like") {
+		message = "The post has been liked!";
+	} else {
+		message = "The post has been unliked!";
+		counter = -1;
+	}
 
-  const post = await Post.findByIdAndUpdate(
-    id,
-    { $inc: { voteCount: counter } },
-    { new: true, upsert: true }
-  );
+	const post = await Post.findByIdAndUpdate(
+		id,
+		{$inc: {voteCount: counter}},
+		{new: true, upsert: true}
+	);
 
-  //console.log(post);
+	//console.log(post);
 
-  //console.log(req.user);
-  if (!post) {
-    return next(
-      new Error(`Post with id: ${req.user._id} is not on the database`, 400)
-    );
-  }
+	//console.log(req.user);
+	if (!post) {
+		return next(new Error(`Post with id: ${req.user._id} is not on the database`, 400));
+	}
 
-  res.status(200).json({ message: message });
+	res.status(200).json({message: message});
 });
 
 /**
@@ -92,23 +88,23 @@ exports.likePost = asyncHandler(async (req, res, next) => {
  * @access   Private
  */
 exports.createPost = asyncHandler(async (req, res, next) => {
-  console.log(req.body);
+	console.log(req.body);
 
-  // finding the required community
-  const community = await Community.find({
-    name: req.body.community.name,
-  }).select(["_id", "tags"]);
-  //console.log(community.tags);
+	// finding the required community
+	const community = await Community.find({
+		name: req.body.community.name,
+	}).select(["_id", "tags"]);
+	//console.log(community.tags);
 
-  // adding required fields
-  req.body.postedBy = mongoose.Types.ObjectId(req.user.id);
-  req.body.community = mongoose.Types.ObjectId(community[0]._id);
-  req.body.tags = [community[0].tags[0]]; // setting the tag to the tag of the community
+	// adding required fields
+	req.body.postedBy = mongoose.Types.ObjectId(req.user.id);
+	req.body.community = mongoose.Types.ObjectId(community[0]._id);
+	req.body.tags = [community[0].tags[0]]; // setting the tag to the tag of the community
 
-  const post = await Post.create(req.body);
-  console.log(post);
+	const post = await Post.create(req.body);
+	console.log(post);
 
-  res.status(200).json({ message: "Your post has been created!" });
+	res.status(200).json({message: "Your post has been created!"});
 });
 
 /**
@@ -117,35 +113,35 @@ exports.createPost = asyncHandler(async (req, res, next) => {
  * @access   Private
  */
 exports.createComment = asyncHandler(async (req, res, next) => {
-  req.body.postedBy = mongoose.Types.ObjectId(req.user.id);
-  req.body.parentPost = mongoose.Types.ObjectId(req.params.postId);
-  req.body.repliedTo = null;
+	req.body.postedBy = mongoose.Types.ObjectId(req.user.id);
+	req.body.parentPost = mongoose.Types.ObjectId(req.params.postId);
+	req.body.repliedTo = null;
 
-  const comment = await Comment.create(req.body);
-  //console.log(comment._id);
+	const comment = await Comment.create(req.body);
+	//console.log(comment._id);
 
-  //Querying the required data
-  const commentResponse = await Comment.find(comment._id)
-    .select(["_id", "content", "asPseudo", "voteCount", "createdAt"])
-    .populate({
-      path: "postedBy",
-      select: "_id name image rank",
-    })
-    .lean();
+	//Querying the required data
+	const commentResponse = await Comment.find(comment._id)
+		.select(["_id", "content", "asPseudo", "voteCount", "createdAt"])
+		.populate({
+			path: "postedBy",
+			select: "_id name image rank",
+		})
+		.lean();
 
-  // editing the createdAt field
-  commentResponse[0].createdAt = getTimeDiff(commentResponse[0].createdAt);
+	// editing the createdAt field
+	commentResponse[0].createdAt = getTimeDiff(commentResponse[0].createdAt);
 
-  //pushing the newly created comment in the post field
-  const updatedPost = await Post.findByIdAndUpdate(
-    req.body.parentPost,
-    { $inc: { commentCount: 1 } },
-    { new: true, upsert: true }
-  );
+	//pushing the newly created comment in the post field
+	const updatedPost = await Post.findByIdAndUpdate(
+		req.body.parentPost,
+		{$inc: {commentCount: 1}},
+		{new: true, upsert: true}
+	);
 
-  console.log(updatedPost);
+	console.log(updatedPost);
 
-  res.status(200).json(commentResponse[0]);
+	res.status(200).json(commentResponse[0]);
 });
 
 /**
@@ -154,33 +150,33 @@ exports.createComment = asyncHandler(async (req, res, next) => {
  * @access   Private
  */
 exports.createReply = asyncHandler(async (req, res, next) => {
-  req.body.postedBy = mongoose.Types.ObjectId(req.user.id);
-  req.body.parentPost = mongoose.Types.ObjectId(req.params.postId);
-  req.body.repliedTo = mongoose.Types.ObjectId(req.params.commentId);
+	req.body.postedBy = mongoose.Types.ObjectId(req.user.id);
+	req.body.parentPost = mongoose.Types.ObjectId(req.params.postId);
+	req.body.repliedTo = mongoose.Types.ObjectId(req.params.commentId);
 
-  const reply = await Comment.create(req.body);
-  //console.log(comment._id);
+	const reply = await Comment.create(req.body);
+	//console.log(comment._id);
 
-  //Querying the required data
-  const replyResponse = await Comment.find(reply._id)
-    .select(["_id", "content", "asPseudo", "voteCount", "createdAt"])
-    .populate({
-      path: "postedBy",
-      select: "_id name image rank",
-    })
-    .lean();
+	//Querying the required data
+	const replyResponse = await Comment.find(reply._id)
+		.select(["_id", "content", "asPseudo", "voteCount", "createdAt"])
+		.populate({
+			path: "postedBy",
+			select: "_id name image rank",
+		})
+		.lean();
 
-  // editing the createdAt field
-  replyResponse[0].createdAt = getTimeDiff(replyResponse[0].createdAt);
+	// editing the createdAt field
+	replyResponse[0].createdAt = getTimeDiff(replyResponse[0].createdAt);
 
-  //console.log(replyResponse);
+	//console.log(replyResponse);
 
-  //pushing the newly created reply in the comment's array of replies
-  await Comment.findByIdAndUpdate(
-    req.params.commentId,
-    { $push: { replies: replyResponse[0]._id } },
-    { new: true, upsert: true }
-  );
+	//pushing the newly created reply in the comment's array of replies
+	await Comment.findByIdAndUpdate(
+		req.params.commentId,
+		{$push: {replies: replyResponse[0]._id}},
+		{new: true, upsert: true}
+	);
 
-  res.status(200).json(replyResponse[0]);
+	res.status(200).json(replyResponse[0]);
 });

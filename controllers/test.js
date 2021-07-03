b076341d53be74e6561fb6496982f1f747154139
AJAkil <cyclosis47@gmail.com>
2021-07-04 02:46:51 +0600

    updated submit test to insert testInfo

--- controllers/test.js
+++ controllers/test.js
@@ -1,6 +1,6 @@
 const Test = require('../models/Test');
 const Advice = require('../models/Advice');
-
+const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
 const mongoose = require('mongoose');
 const ErrorResponse = require('../utils/errorResponse');
@@ -72,7 +72,6 @@ exports.submitTest = asyncHandler(async (req, res) => {
   const depressionScore = parseInt(req.body.depression);
   const stressScore = parseInt(req.body.stress);
 
-  //console.log(typeof anxietyScore);
 
   const anxietyAdvice = await Advice.find({
     test: { _id: req.params.testId },
@@ -81,7 +80,7 @@ exports.submitTest = asyncHandler(async (req, res) => {
       { 'range.max': { $gt: anxietyScore } },
       { 'range.min': { $lt: anxietyScore } },
     ],
-  }).select(['messages', 'advice']);
+  }).select(['_id','messages', 'advice']);
 
   const depressionAdvice = await Advice.find({
     test: { _id: req.params.testId },
@@ -90,7 +89,7 @@ exports.submitTest = asyncHandler(async (req, res) => {
       { 'range.max': { $gt: depressionScore } },
       { 'range.min': { $lt: depressionScore } },
     ],
-  }).select(['messages', 'advice']);
+  }).select(['_id', 'messages', 'advice']);
 
   const stressAdvice = await Advice.find({
     test: { _id: req.params.testId },
@@ -99,7 +98,7 @@ exports.submitTest = asyncHandler(async (req, res) => {
       { 'range.max': { $gt: stressScore } },
       { 'range.min': { $lt: stressScore } },
     ],
-  }).select(['messages', 'advice']);
+  }).select(['_id', 'messages', 'advice']);
 
   const responseObject = {
     _id: req.params.testId,
@@ -107,6 +106,64 @@ exports.submitTest = asyncHandler(async (req, res) => {
     depressionAdvice: depressionAdvice,
     stressAdvice: stressAdvice,
   };
+
+  // add latest advices to user's array
+
+  // calculate status based on score range
+  let anxietyStatus, stressStatus, depressionStatus, status;
+  
+  // anxietyStatus
+  if( anxietyScore >= 0 && anxietyScore <= 7){
+    anxietyStatus = 1;
+  }else if( anxietyScore >= 8 && anxietyScore <= 14 ){
+    anxietyStatus = 2;
+  }else if(anxietyScore >= 15 && anxietyScore <= 20){
+    anxietyStatus = 3;
+  }
+
+  if( depressionScore >= 0 && depressionScore <= 7){
+    depressionStatus = 1;
+  }else if( depressionScore >= 8 && depressionScore <= 14 ){
+    depressionStatus = 2;
+  }else if(depressionScore >= 15 && depressionScore <= 20){
+    depressionStatus = 3;
+  }
+
+  if( stressScore >= 0 && stressScore <= 7){
+    stressStatus = 1;
+  }else if( stressScore >= 8 && stressScore <= 14 ){
+    stressStatus = 2;
+  }else if(stressScore >= 15 && stressScore <= 20){
+    stressStatus = 3;
+  }
+
+  const sum = anxietyStatus + depressionStatus + stressStatus;
+
+  if(sum === 3){
+    status = 'excellent';
+  }else if(sum === 4 || sum === 5 ){
+    status = 'good';
+  }else if(sum === 6 || sum === 7){
+    status = 'bad';
+  }else if(sum === 8 || sum == 9){
+    status = 'concerning'; 
+  }
+
+
+  // construct the object and then push
+  const testInfo = {
+      test: req.params.testId,
+      anxietyScore: anxietyScore,
+      stressScore: stressScore,
+      depressionScore: depressionScore,
+      status: status,
+      advice: [anxietyAdvice[0]._id, depressionAdvice[0]._id, stressAdvice[0]._id]
+  }
+
+  const user = await User.findOne(req.user._id);
+  user.testInfo.unshift(testInfo);
+
+  await user.save()
 
   res.status(200).json(responseObject);
 });

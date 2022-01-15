2e552bb08b388709f9fa498ff99b7cfc85721dc8
unknown <imshmen077@gmail.com>
2022-01-15 20:37:50 +0600

    E to e

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('./async');
-const ErrorResponse = require('../utils/ErrorResponse');
+const ErrorResponse = require('../utils/errorResponse');
 const User = require('../models/User');
 
 exports.protect = asyncHandler(async (req, res, next) => {
